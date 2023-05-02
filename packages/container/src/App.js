895ad130ca66d7af6f1f529c3ae7ff53594b06ae
Danillo Ewerton Pereira Torres <danilloept@gmail.com>
2023-05-01 21:32:24 -0300

    Add files via upload

---
+++ packages/container/src/App.js
@@ -0,0 +1,41 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import Header from "./components/Header";
+import Progress from "./components/Progress";
+import { useState } from "react";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
+export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,25 @@
+import { mount } from "auth/AuthApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default ({ onSignIn }) => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { pathname } = history.location;
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+      onSignIn,
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
+};
